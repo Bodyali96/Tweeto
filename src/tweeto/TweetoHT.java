3c6e050e94aa7e1699a36ece8c4c0e91ada41b21
bodyali96 <mboda96@yahoo.com>
2017-12-14 18:18:25 +0300

    hash table

---
+++ src/tweeto/TweetoHT.java
@@ -0,0 +1,16 @@
+package tweeto;
+
+public class TweetoHT {
+    // a class to hold all the tweets inside a hash table implemented as an array.
+
+    Tweeto HT[];
+    int size;
+    //initialize the variables
+    public TweetoHT(){ HT = new Tweeto[36]; size = 0;}
+    public void insert(){
+
+    }
+
+
+
+}
