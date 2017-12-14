2ed30a55cde70e09a54e7c8065f0452cc5f85034
bodyali96 <mboda96@yahoo.com>
2017-12-14 18:15:12 +0300

    creating hash table

--- src/tweeto/Test.java
+++ src/tweeto/Test.java
@@ -7,47 +7,41 @@ import java.util.Scanner;
 
 public class Test {
     
-    public static void main(String [] args) throws Exception{
-//        FileReader file = new FileReader("1000.txt");
-//        Scanner scan = new Scanner(file);
-//        ArrayList <Tweeto> tw = new ArrayList<Tweeto>();
-//        int count = 0;
-//        while(scan.hasNext()){
-//            String line = scan.nextLine();
-//            if(!line.contains("Wed Dec"))
-//                while(!line.contains("Wed Dec"))
-//                    line = line.concat(scan.nextLine());
-//            tw.add(new Tweeto(line));
-//            System.out.println(tw.get(count));
-//            count++;
-//        }
-//        scan.close();
-//        System.out.println(count);
+    public static void main(String [] args){
+        try{
 
-        String arr[] = new String[37];
-        String letter = "Z";
-        int place = letter.charAt(0) - '0' - 7;
-        arr[place] = letter;
-        System.out.println(arr[place]+ " "+ place);
-        String m = "3";
-        // dasdasda sd
+            FileReader file = new FileReader("1000.txt");
+            Scanner scan = new Scanner(file);
+            while(scan.hasNext()){
+                String line = scan.nextLine();
+                if (!line.contains("Wed Dec"))
+                    while (!line.contains("Wed Dec"))
+                        line = line.concat(scan.nextLine());
+            }
+            scan.close();
 
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void usingArrayList()throws Exception {
+        // this function to read the txt file and store the info to a simple array list
+        FileReader file = new FileReader("1000.txt");
+        Scanner scan = new Scanner(file);
+        ArrayList<Tweeto> tw = new ArrayList<Tweeto>();
+        int count = 0;
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            if (!line.contains("Wed Dec"))
+                while (!line.contains("Wed Dec"))
+                    line = line.concat(scan.nextLine());
+            tw.add(new Tweeto(line));
+            System.out.println(tw.get(count));
+            count++;
+        }
+        scan.close();
+        System.out.println(count);
     }
 }
--- src/tweeto/Tweeto.java
+++ src/tweeto/Tweeto.java
@@ -2,22 +2,28 @@
 package tweeto;
 
 public class Tweeto {
+    // this variable to store the ID #Note : started with @
     private String ID;
+    // this variable to store the content of the tweet itself, including the mentions/RT
     private String Tweet;
+    // this variable is the link to the next Tweeto object in the hash table
     private Tweeto next;
+    // this is just a line used to read a single line each time from the txt file
     private String line = "";
-    
+
+    // default constructor
     public Tweeto(){ ID = Tweet = null ; next = null;}
 
+    // full argument constructor using setters
     public Tweeto(String line){
        setLine(line);
        setID();
        setTweet();
     }
 
-    public void setLine(String line) {
+    // A function to convert the whole line to a new line without the numbers in the beginning
+    private void setLine(String line) {
         this.line = line.substring(line.indexOf("@"));
-
     }
 
     public String getLine(){ return this.line;}
@@ -26,6 +32,7 @@ public class Tweeto {
         return ID;
     }
 
+    // using String methods to trim the ID that starts with @ until the white space
     private void setID() {
         this.ID = getLine().substring(0,getLine().indexOf(" "));
     }
@@ -33,8 +40,11 @@ public class Tweeto {
     public String getTweet() {
         return Tweet;
     }
-
+    // storing the tweet's content without the date in the end
     private void setTweet() {
+        if(getLine().contains("RT @"))
+            this.Tweet = line.substring(line.indexOf(":")+2,line.indexOf("Wed Dec"));
+        else if(true);
         this.Tweet = line.substring(0,line.indexOf("Wed Dec"));
     }
 
@@ -42,10 +52,12 @@ public class Tweeto {
         return next;
     }
 
+    // this method will used in binary heap or hash tables, but it won't used in array list
     public void setNext(Tweeto next) {
         this.next = next;
     }
 
+    // A string representation for the object
     @Override
     public String toString() {
         return String.format("%s %s",ID,Tweet);
